2776a7ca48bfba24cec221f289e30baaaefdebe4
lemonzap <adrian.macleod.jr@gmail.com>
2020-11-12 22:38:08 -0800

    Created Security Camera

---
+++ Assets/LemonzapUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LemonzapUtils
+{
+    public class Utils
+    {
+        public static Vector3 GetVectorFromAngle(float angle)
+        {
+            float angleRad = angle * (Mathf.PI / 180f);
+            return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+        }
+
+    }
+}
